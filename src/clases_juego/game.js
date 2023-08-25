a51cb577297664cfc1a46fbddfa700be99953e32
Requiem4U <cmcclcdz1@gmail.com>
2023-08-25 12:35:52 -0600

    Mov Diagonal Personaje

--- src/clases_juego/game.js
+++ src/clases_juego/game.js
@@ -2,6 +2,8 @@ import Phaser from "phaser";
 import backgroundImg from '../imagenes/fondo1.jpg'
 import spritesheetP_M from '../imagenes/Mujer_Style_Sheets_128x128.png'
 
+const v_m_personaje = 160
+
 export class Game extends Phaser.Scene {
 
   constructor() {
@@ -15,12 +17,12 @@ export class Game extends Phaser.Scene {
   create() {
     const gameContainer = document.getElementById('contenedor_juego');
 
-    this.player = this.physics.add.sprite(gameContainer.clientWidth / 2, gameContainer.clientHeight / 2, 'player_M')
+    this.player = this.physics.add.sprite(this.game.canvas.width / 2, this.game.canvas.height / 2, 'player_M')
 
-    crearAnimacionCaminata(this,'player_M' , 'walkDown', 0, 3);
-    crearAnimacionCaminata(this,'player_M' , 'walkUp', 4, 7);
-    crearAnimacionCaminata(this,'player_M' , 'walkLeft', 8, 11);
-    crearAnimacionCaminata(this,'player_M' , 'walkRight', 12, 15);
+    crearAnimacionCaminata(this, 'player_M', 'walkDown', 0, 3);
+    crearAnimacionCaminata(this, 'player_M', 'walkUp', 4, 7);
+    crearAnimacionCaminata(this, 'player_M', 'walkLeft', 8, 11);
+    crearAnimacionCaminata(this, 'player_M', 'walkRight', 12, 15);
 
     this.physics.world.enable(this.player);
     this.player.setCollideWorldBounds(true);
@@ -29,19 +31,49 @@ export class Game extends Phaser.Scene {
   update() {
     const cursors = this.input.keyboard.createCursorKeys();
 
-  this.player.setVelocity(0);
+    this.player.setVelocity(0);
 
-  if (cursors.right.isDown) {
-    handlePlayerMovement(this.player, 160, 0, 'walkRight');
-  } else if (cursors.left.isDown) {
-    handlePlayerMovement(this.player, -160, 0, 'walkLeft');
-  } else if (cursors.up.isDown) {
-    handlePlayerMovement(this.player, 0, -160, 'walkUp');
-  } else if (cursors.down.isDown) {
-    handlePlayerMovement(this.player, 0, 160, 'walkDown');
-  } else {
-    this.player.anims.stop();
-  }
+    // Ajustar la velocidad diagonal
+    const diagonalVelocity = v_m_personaje * Math.sqrt(0.5);
+
+    // Evaluación de los distinos casos en los que se precionan los botones de movimiento
+    switch (true) {
+      case cursors.up.isDown && cursors.left.isDown: // Teclas arriba e izquierda
+        handlePlayerMovement(this.player, -diagonalVelocity, -diagonalVelocity, 'walkUp');
+        break;
+    
+      case cursors.up.isDown && cursors.right.isDown: // Teclas arriba y derecha
+        handlePlayerMovement(this.player, diagonalVelocity, -diagonalVelocity, 'walkUp');
+        break;
+    
+      case cursors.down.isDown && cursors.left.isDown: // Teclas abajo e izquierda
+        handlePlayerMovement(this.player, -diagonalVelocity, diagonalVelocity, 'walkDown');
+        break;
+    
+      case cursors.down.isDown && cursors.right.isDown: // Teclas abajo y derecha
+        handlePlayerMovement(this.player, diagonalVelocity, diagonalVelocity, 'walkDown');
+        break;
+    
+      case cursors.right.isDown: // Tecla derecha
+        handlePlayerMovement(this.player, v_m_personaje, 0, 'walkRight');
+        break;
+    
+      case cursors.left.isDown: // Tecla izquierda
+        handlePlayerMovement(this.player, -v_m_personaje, 0, 'walkLeft');
+        break;
+    
+      case cursors.down.isDown: // Tecla abajo
+        handlePlayerMovement(this.player, 0, v_m_personaje, 'walkDown');
+        break;
+    
+      case cursors.up.isDown: // Tecla arriba
+        handlePlayerMovement(this.player, 0, -v_m_personaje, 'walkUp');
+        break;
+    
+      default: // Ninguna tecla
+        this.player.anims.stop();
+    }
+    
   }
 }
 
--- src/clases_juego/index.js
+++ src/clases_juego/index.js
@@ -1,30 +1,35 @@
 import { Game } from './game.js';
 
-const gameContainer = document.getElementById('contenedor_juego');
+window.onload = ()=>{
 
-const config = {
-  type: Phaser.AUTO,
-  width: '100%',
-  height: '100%',
-  parent: 'contenedor_juego',
-  scene: [Game],
-  physics: {
-    default: 'arcade',
-    arcade: {
-      gravity: { y: 0 },
-      debug: false
+  let gameContainer = document.getElementById('contenedor_juego');
+
+  const config = {
+    type: Phaser.AUTO,
+    width: '100vw',
+    height: '100vh',
+    parent: 'contenedor_juego',
+    scene: [Game],
+    physics: {
+      default: 'arcade',
+      arcade: {
+        gravity: { y: 0 },
+        debug: false
+      }
     }
   }
+
+  let game = new Phaser.Game(config);
+
+  function resizeGame() {
+    console.log(gameContainer)
+    const newWidth = gameContainer.clientWidth;
+    const newHeight = gameContainer.clientHeight;
+
+    game.renderer.resize(newWidth, newHeight);
+  }
+
+  window.addEventListener('resize', resizeGame);
+
+  resizeGame
 }
-
-let game = new Phaser.Game(config);
-
-function resizeGame() {
-  console.log(gameContainer)
-  const newWidth = gameContainer.clientWidth;
-  const newHeight = gameContainer.clientHeight;
-
-  game.renderer.resize(newWidth, newHeight);
-}
-
-window.addEventListener('resize', resizeGame);
